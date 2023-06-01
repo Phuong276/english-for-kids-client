b46f91ee30d1ef9fd398cd37f1170c9d56081a18
phuongvd-dipro <phuongvd@dipro.vn>
2023-06-01 15:05:46 +0700

    fix: css

--- src/components/GrammarGame/Questions.js
+++ src/components/GrammarGame/Questions.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 export default function QuestionsGrammarGame(props) {
   const { question, trace } = props;
 
-  const listitems = question?.answers;
+  const listitems = question?.answers || [{}, {}];
   listitems.map((item) => (item.id = String(item.id)));
 
   const [list, setList] = useState(listitems);
@@ -54,65 +54,69 @@ export default function QuestionsGrammarGame(props) {
   props.callbackSetAnswerText(arrayTextView.join(" "));
   return (
     <div>
-      <div className="border-[7px] border-green-600 bg-green-300 rounded-3xl">
-        <div className="text-5xl font-thin pt-6 pb-6">
-          <b>
-            {arrayTextView.join(" ") === ""
-              ? question?.questionText
-              : arrayTextView.join(" ")}
-          </b>
-        </div>
-        <img
-          src={question.questionImage}
-          alt={question.id}
-          width={400}
-          height={300}
-          className="mx-auto rounded-[20%]"
-        />
-        <div className="grid grid-cols-2 gap-1 mb-10">
-          <div
-            className="p-4 mt-5 bg-white rounded-lg shadow-lg w-[700px]"
-            onDragOver={handleDragOver}
-            onDrop={handleDrop2}
-          >
-            <ul className="list-none p-0 m-0 bg-green-200 border border-green-300 min-h-40 text-2xl">
-              {list.map((item) => (
-                <li
-                  key={item.id}
-                  id={item.id}
-                  className="bg-white border border-green-300 p-4 mb-2 cursor-move"
-                  draggable={true}
-                  onDragStart={handleDragStart}
-                >
-                  {item.answerText}
-                </li>
-              ))}
-            </ul>
+      {question ? (
+        <div className="border-[7px] border-green-600 bg-green-300 rounded-3xl">
+          <div className="text-5xl font-thin pt-6 pb-6">
+            <b>
+              {arrayTextView.join(" ") === ""
+                ? question?.questionText
+                : arrayTextView.join(" ")}
+            </b>
           </div>
-          <div
-            className={`p-4 mt-4 bg-white rounded-lg shadow-lg border-dashed border-2 min-h-60 ${
-              isDragging ? "border-black" : "border-indigo-300 text-2xl"
-            } `}
-            onDragOver={handleDragOver}
-            onDrop={handleDrop}
-          >
-            <p>Sort answers here</p>
-            <ul className="list-none p-0 m-0 bg-green-200 border border-green-300 min-h-40">
-              {draggedList.map((item) => (
-                <li
-                  key={item.id}
-                  id={item.id}
-                  className="bg-white border border-green-300 p-4 mb-2 cursor-move"
-                  draggable={true}
-                  onDragStart={handleDragStart}
-                >
-                  {item.answerText}
-                </li>
-              ))}
-            </ul>
+          <img
+            src={question.questionImage}
+            alt={question.id}
+            width={400}
+            height={400}
+            className="mx-auto rounded-[20%]"
+          />
+          <div className="grid grid-cols-2 gap-1 mb-10">
+            <div
+              className="p-4 mt-5 bg-white rounded-lg shadow-lg w-[700px]"
+              onDragOver={handleDragOver}
+              onDrop={handleDrop2}
+            >
+              <ul className="list-none p-0 m-0 bg-green-200 border border-green-300 min-h-40 text-2xl">
+                {list.map((item) => (
+                  <li
+                    key={item.id}
+                    id={item.id}
+                    className="bg-white border border-green-300 p-4 mb-2 cursor-move"
+                    draggable={true}
+                    onDragStart={handleDragStart}
+                  >
+                    {item.answerText}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div
+              className={`p-4 mt-4 bg-white rounded-lg shadow-lg border-dashed border-2 min-h-60 ${
+                isDragging ? "border-black" : "border-indigo-300 text-2xl"
+              } `}
+              onDragOver={handleDragOver}
+              onDrop={handleDrop}
+            >
+              <p>Sort answers here</p>
+              <ul className="list-none p-0 m-0 bg-green-200 border border-green-300 min-h-40">
+                {draggedList.map((item) => (
+                  <li
+                    key={item.id}
+                    id={item.id}
+                    className="bg-white border border-green-300 p-4 mb-2 cursor-move"
+                    draggable={true}
+                    onDragStart={handleDragStart}
+                  >
+                    {item.answerText}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div>No question</div>
+      )}
     </div>
   );
 }
