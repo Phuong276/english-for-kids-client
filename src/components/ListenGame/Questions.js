c4877cec351e5e9a28293a37cf4da385ae1706be
phuongvd-dipro <phuongvd@dipro.vn>
2023-06-07 16:48:03 +0700

    css

--- src/components/ListenGame/Questions.js
+++ src/components/ListenGame/Questions.js
@@ -15,7 +15,7 @@ export default function QuestionListenGame(props) {
         onClick={playAudio}
         src={question.questionImage}
         alt={question.id}
-        className="hover:grow hover:shadow-lg rounded-[20%] hover:animate-pulse duration-200 w-[400px] h-[300px]"
+        className="hover:grow hover:shadow-lg rounded-[20%] hover:animate-pulse duration-200 w-[400px] h-[300px] cursor-pointer"
       />
     </div>
   );
